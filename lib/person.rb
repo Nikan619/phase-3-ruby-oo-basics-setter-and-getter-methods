3f8b6f7ec8012ba33a99a14e338e5b1304dc83a9
Nikan <njavadzadeh@gmail.com>
2022-11-23 08:44:30 -0800

    commit tree 1eb421512acf306d12bfda870df311e49123aaff

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,24 @@
+require 'pry'
+
 class Person
 
+attr_accessor :name,:job
+# def name=(name)
+#     @name = name
+# end
+# def job=(job)
+#     @job = job
+# end
+# def name
+#     @name
+# end
+# def job
+#     @job
+def name_upcase
+@name.upcase
+def new_name(newname)
+    self.name = newname
 end
+end
+binding.pry
+
